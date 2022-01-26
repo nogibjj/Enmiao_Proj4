915f848979a9ec9beff2de27aadc1427d8c944be
EnzooF <ef141@duke.edu>
2022-01-25 22:22:46 -0500

    tishi

--- api.py
+++ api.py
@@ -7,7 +7,12 @@ app = FastAPI()
 predict = SentimentIntensityAnalyzer()
 
 #need input!
+
 @app.get("/")
+def hello():
+    return "query format: 0.0.0.0:8000/predict?text="
+
+@app.get("/predict")
 def fetch_predictions(text: str):
     result = predict.polarity_scores(text)
     return result
